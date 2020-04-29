0808f537a8708f58dce2d22ea66fd75e8141f635
Ben D <33376747+BenD@users.noreply.github.com>
2020-04-29 12:53:17 +0100

    assignment 4

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// Filter table by search
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
@@ -7,6 +8,7 @@ $(document).ready(function(){
   });
 });
 
+// For table colouring if late, <= 20 mins, <= 10 mins
 function ti()
 {
     var table = document.getElementById('myTable');
@@ -15,7 +17,7 @@ function ti()
     {
         for (var j = 3; j < table.rows[i].cells.length - 3; j++)
         {
-          time_until = table.rows[i].cells[5].innerHTML
+          var time_until = table.rows[i].cells[5].innerHTML
           if (time_until < 21 && time_until > 10)
           {
             table.rows[i].cells[5].style["background-color"] = "orange";
@@ -28,5 +30,31 @@ function ti()
           }
        }
     }
+
+    // For Time status
+    for (var i = 1; i < table.rows.length; i++)
+    {
+      var time_until1 = table.rows[i].cells[6].innerHTML
+
+      if(time_until1 == 0)
+      {
+        table.rows[i].cells[6].innerHTML = "On time"
+      }
+
+      else if(time_until1 < 0)
+      {
+        table.rows[i].cells[6].innerHTML = `Early (${time_until1} minutes)`
+      }
+
+      else
+      {
+        table.rows[i].cells[6].innerHTML < 2 ? table.rows[i].cells[6].innerHTML = `Late (+ ${time_until1} minute)`
+        :
+        table.rows[i].cells[6].innerHTML = `Late (+ ${time_until1} minutes)`
+      }
+    }
+
 }
 ti();
+
+
