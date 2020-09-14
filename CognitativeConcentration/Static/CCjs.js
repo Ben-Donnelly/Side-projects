c52bb49de4a2e3b9db824f2d2d6c8c08252ef67f
Ben D <33376747+BenD@users.noreply.github.com>
2020-09-14 17:17:40 +0100

    added more functionl]ality and seperated into different directory

---
+++ CognitativeConcentration/Static/CCjs.js
@@ -0,0 +1,67 @@
+function timer()
+{
+    var startTime = Date.now();
+
+    interval = setInterval(function() {
+        var elapsedTime = Date.now() - startTime;
+        document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
+    }, 100);
+
+}
+
+
+
+function ti()
+{
+    var table = document.getElementById('myTable');
+    for (var i = 0; i < table.rows.length; i++)
+    {
+        for (var j = 0; j < table.rows[i].cells.length; j++)
+        {
+            table.rows[i].cells[j].style["background-color"] = "orange";
+        }
+   }
+}
+count = 1;
+$( "td" ).click(function() {
+
+
+    if ($(this).text() == count)
+    {
+        if (count == 1)
+        {
+            timer();
+        }
+
+        if (count == 16)
+        {
+        clearInterval(interval);
+        x = 0
+        while(x < 3)
+        {
+        console.log(x);
+        $('#timer').fadeOut(500).fadeIn(500);
+        x++;
+        }
+
+        }
+
+
+        console.log(count);
+        $(this).css('backgroundColor', 'grey');
+        $(this).html('');
+
+        if (count == 16)
+        {
+            console.log('fuck');
+        }
+
+        count++;
+    }
+
+
+
+});
+//ti();
+
+
