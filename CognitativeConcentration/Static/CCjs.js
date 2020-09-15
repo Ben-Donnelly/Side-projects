d54d5f8e6ad5ce00290f088d0d0261f90ab6e73e
Ben D <33376747+BenD@users.noreply.github.com>
2020-09-15 10:20:17 +0100

    cleaned up code

--- CognitativeConcentration/Static/CCjs.js
+++ CognitativeConcentration/Static/CCjs.js
@@ -1,67 +1,82 @@
+//document.getElementById('myTable').style.filter = "blur(150px)";
+
+$(document).ready(function()
+{
+  document.getElementById('myTable').style.filter = "blur(5px)";
+});
+
+let func = function()
+{
+  var timeleft = 3;
+  var downloadTimer = setInterval(function()
+  {
+    if (timeleft <= 0)
+    {
+      document.getElementById("countdown").innerHTML = ""
+      document.getElementById('myTable').style.filter = "unset";
+      timer();
+    }
+    else
+    {
+      document.getElementById("countdown").innerHTML = timeleft;
+    }
+    timeleft -= 1;
+  }, 1000);
+
+}
+
+
+let elem = $('#MyB');
+
+elem.one('click', func);
+
+
 function timer()
 {
-    var startTime = Date.now();
+  var startTime = Date.now();
 
-    interval = setInterval(function() {
-        var elapsedTime = Date.now() - startTime;
-        document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
-    }, 100);
-
+  interval = setInterval(function()
+  {
+    var elapsedTime = Date.now() - startTime;
+    document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
+  }, 100);
 }
 
 
 
 function ti()
 {
-    var table = document.getElementById('myTable');
-    for (var i = 0; i < table.rows.length; i++)
+  var table = document.getElementById('myTable');
+  for (var i = 0; i < table.rows.length; i++)
+  {
+    for (var j = 0; j < table.rows[i].cells.length; j++)
     {
-        for (var j = 0; j < table.rows[i].cells.length; j++)
-        {
-            table.rows[i].cells[j].style["background-color"] = "orange";
-        }
-   }
+      table.rows[i].cells[j].style["background-color"] = "orange";
+    }
+  }
 }
+
 count = 1;
-$( "td" ).click(function() {
-
-
-    if ($(this).text() == count)
+$("td").click(function()
+{
+  if ($(this).text() == count)
+  {
+    if (count == 16)
     {
-        if (count == 1)
-        {
-            timer();
-        }
-
-        if (count == 16)
-        {
-        clearInterval(interval);
-        x = 0
-        while(x < 3)
-        {
+      clearInterval(interval);
+      x = 0
+      while (x < 3)
+      {
         console.log(x);
         $('#timer').fadeOut(500).fadeIn(500);
         x++;
-        }
-
-        }
-
-
-        console.log(count);
-        $(this).css('backgroundColor', 'grey');
-        $(this).html('');
-
-        if (count == 16)
-        {
-            console.log('fuck');
-        }
-
-        count++;
+      }
     }
+    console.log(count);
+    $(this).css('backgroundColor', 'grey');
+    $(this).html('');
 
-
-
+    count++;
+  }
 });
-//ti();
-
 
